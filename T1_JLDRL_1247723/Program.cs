0db1c811ac4747bbc5b00a3e56f5067448705ba7
04dan <04dan@LAPTOP-35D3RKBD>
2023-02-18 11:26:02 -0600

    Agregar archivos de proyecto.

---
+++ T1_JLDRL_1247723/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T1_JLDRL_1247723
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+
+            Console.WriteLine("Ingrese nombre: ");
+            String sNombre = Console.ReadLine();
+            Console.WriteLine("Ingrese edad: ");
+            String sEdad = Console.ReadLine();
+            Console.WriteLine("Ingrese carrera: ");
+            String sCarrera = Console.ReadLine();
+            Console.WriteLine("Ingrese carné: ");
+            String sCarne = Console.ReadLine();
+            Console.ReadKey();
+
+            Console.WriteLine("Nombre: " + sNombre);
+
+            Console.WriteLine("Edad: " + sEdad);
+ 
+            Console.WriteLine("Carrera: " + sCarrera);
+
+            Console.WriteLine("Carné: " + sCarne);
+
+            Console.WriteLine("Soy " + sNombre + ", tengo " + sEdad + " años y estudio la carrera de " + sCarrera + ". Mi número de carné es; " + sCarne);
+            Console.ReadKey();
+
+
+        }
+    }
+}
